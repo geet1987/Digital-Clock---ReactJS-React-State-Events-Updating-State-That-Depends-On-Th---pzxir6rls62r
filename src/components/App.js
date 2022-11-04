82091153d8926e39d2463cb4deaafd03ae1670f6
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-05 00:03:58 +0530

    Update App.js
    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
+  
+  let time = new Date().toLocaleTimeString();
+  const [ctime,setCtime]=useState();
+
+  const UpdateTime = () => {
+time =new Date().toLocaleTimeString();
+setCtime(time);
+  };
+
+  setInterval(UpdateTime,1000);
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">
+    
+    <h1>{time}</h1>
+<button onClick={UpdateTime}>get time</button>
+
+    </div>
     </div>
   )
 }
